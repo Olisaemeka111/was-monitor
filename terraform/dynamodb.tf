bbb006868aebb6837e84db020bb94d2b11f484c4
olisaemeka111 <olisa.arinze1@aol.com>
2025-03-30 21:11:30 +0100

    fix: Add stream_view_type to DynamoDB table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -4,6 +4,7 @@ resource "aws_dynamodb_table" "aws_accounts" {
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "account_id"
   stream_enabled = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "account_id"
