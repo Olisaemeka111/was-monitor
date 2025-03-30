c593db1b0e18700a69be6086a1a6592075f84344
olisaemeka111 <olisa.arinze1@aol.com>
2025-03-30 20:34:13 +0100

    feat: Configure Terraform remote state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket         = "aws-monitor-tf-state-156041437006"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-monitor-tf-lock-156041437006"
+    encrypt        = true
+  }
+}
 
 provider "aws" {
   region = "us-east-1"
